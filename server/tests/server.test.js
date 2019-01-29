ae8afcda6f4fdd19d02ae68922f5d030130562db
SJ <vince15dk@gmail.com>
2019-01-29 20:41:56 +0900

    getId test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 },{
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -75,4 +78,32 @@ describe('Get /todos',()=>{
         })
         .end(done);
     })
+})
+
+describe('Get /todos/:id',()=>{
+    it('should get todo',(done)=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect(res=>{
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    })
+
+    it('should return 404 if todo not found', (done)=>{
+        const hexId = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+    })
+
+    it('should return 404 for non-object ids', (done)=>{
+        request(app)
+        .get('/todos/123abc')
+        .expect(404)
+        .end(done);
+    })
+
 })
